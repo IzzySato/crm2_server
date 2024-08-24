006edbbcaac585106de9502d81c8ebc18eaabd53
IzzySato <izzy.sato@gmail.com>
2024-08-24 10:14:32 -0700

    disabled generate token for production

--- routes/jwt/index.js
+++ routes/jwt/index.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const { generateJwtToken } = require('../../lib/jwt');
 
 router.post('/generate-token', async (req, res, next) => {
+  if (process.env.NODE_ENV !== 'dev') {
+    res.json({ message: 'dev only endpoint'});
+    return;
+  }
   const { userId } = req.body;
   const token = generateJwtToken(userId);
   res.json({ token });
